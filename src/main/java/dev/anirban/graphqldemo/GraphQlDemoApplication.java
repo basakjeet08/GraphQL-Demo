1ca2f1f57acec014bf82878a6474c38ebc3206e0
Anirban Basak <jeetbasak2002@gmail.com>
2024-11-12 19:46:10 +0530

    feat - Configured Database connection to my sql.

--- src/main/java/dev/anirban/graphqldemo/GraphQlDemoApplication.java
+++ src/main/java/dev/anirban/graphqldemo/GraphQlDemoApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GraphQlDemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GraphQlDemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlDemoApplication.class, args);
+    }
 }
